d8b39c2d6236c344ebb3ee91515551bcf1bb591d
siyam <kazikiwi55@gmail.com>
2020-08-25 20:47:09 -0400

    add images of cars ground and track

--- sketch.js
+++ sketch.js
@@ -6,9 +6,26 @@ var allPlayers;
 var distance = 0;
 var database;
 
+var car1img,car2img,car3img,car4img;
+
+var track;
+
+var ground;
+
 var form, player, game;
 
 var cars, car1, car2, car3, car4;
+
+function preload(){
+  car1img = loadImage("../images/car1.png");
+  car2img = loadImage("../images/car2.png");
+  car3img = loadImage("../images/car3.png");
+  car4img = loadImage("../images/car4.png");
+
+  track = loadImage("../images/track.jpg");
+  ground = loadImage("../images/ground.png");
+}
+
 
 
 function setup(){
@@ -19,7 +36,6 @@ function setup(){
   game.start();
 }
 
-
 function draw(){
   if(playerCount === 4){
     game.update(1);
@@ -27,5 +43,8 @@ function draw(){
   if(gameState === 1){
     clear();
     game.play();
+  }
+  if(gameState ===2){
+    game.end();
   }
 }
